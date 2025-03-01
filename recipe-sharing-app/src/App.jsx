ca28a3784d297c3e03fbd3f3e48b7445eca0820d
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-03-01 13:28:18 +0100

    Task3

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-// import RecipeDetails from './components/RecipeDetails';
-// import FavoritesList from './components/FavoritesList';
-// import RecommendationsList from './components/RecommendationsList';
+import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
